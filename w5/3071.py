8410282a4e723b572275efd9558d70e96f7ff86c
Ton-1010 <69070319@kmitl.ac.th>
2026-08-27 22:10:48 +0700

    Create 3071.py

---
+++ w5/3071.py
@@ -0,0 +1,10 @@
+"""จำนวนในช่วง[A,B]ที่หารด้วย b เหลือเศษ r"""
+a = int(input())
+b = int(input())
+d = int(input())
+r = int(input())
+count = 0
+for i in range(a,b+1):
+    if i % d == r:
+        count += 1
+print(count)
